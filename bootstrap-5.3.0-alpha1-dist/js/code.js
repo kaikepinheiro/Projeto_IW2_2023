7afd9b9d524ffb3335976da9709aeef3c012402d
Kaike Pinheiro Simplicio <100208435+projkaikepinheiro@users.noreply.github.com>
2023-03-09 22:41:10 -0300

    Add files via upload

---
+++ bootstrap-5.3.0-alpha1-dist/js/code.js
@@ -0,0 +1,16 @@
+
+function sobre(){
+    window.location.href = "index.html"
+}
+function funcionamento(){
+    window.location.href = "funcionamento.html"
+}
+function exemplos(){
+    window.location.href = "exemplos.html"
+}
+function vantagens(){
+    window.location.href = "beneficios.html"
+}
+function login(){
+    window.location.href = "login.html"
+}
